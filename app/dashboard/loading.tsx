aeb869a9c9c9a32547b5eb3beaa94e6e943b2ef2
shafiyaali <shafiyaali99@gmail.com>
2026-07-13 21:17:17 +0300

    Implemented database integration, dashboard and invoices page, search and pagination functionality

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import DashboardSkeleton from '../ui/skeletons'
+const Loading = () => {
+  return (
+  //  <DashboardSkeleton />
+
+  <div> Loading..</div>
+  )
+}
+
+export default Loading

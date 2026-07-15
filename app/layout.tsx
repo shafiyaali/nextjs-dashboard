a1825c327beaa2624e93689cb189b8ea77def286
shafiyaali <shafiyaali99@gmail.com>
2026-07-15 14:14:40 +0300

    Added metaData for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import  {inter } from "@/app/ui/fonts"
 import "@/app/ui/global.css"
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Shafiya Invoice Dashboard",
+  description: "Shafiya built this invoice dashboard for learning",
+  keywords: "shafiya, shinaash, invoice, dashboard"
+}
 export default function RootLayout({
   children,
 }: {
